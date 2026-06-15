e59ef52e43e4d96034442a31733243f68362b8f1
Tsubasa <m.tsubasa0512@gmail.com>
2026-06-15 12:27:05 +0900

    基本ノーツを用意した

---
+++ Project/Note/Long/LongNote.cpp
@@ -0,0 +1,20 @@
+#include "LongNote.h"
+
+LongNote::LongNote(){
+
+}
+
+void LongNote::Initialize(const uint32_t modelHandle){
+	//モデルの生成
+	model_=Elysia::Model::Create(modelHandle);
+}
+
+void LongNote::Update()
+{
+}
+
+void LongNote::DrawObject3D(const Camera& camera)
+{
+
+	//model_->Draw()
+}
---
+++ Project/Note/Long/LongNote.h
@@ -0,0 +1,47 @@
+#pragma once
+
+/**
+ * @file LongNote.h
+ * @brief ロングノーツ
+ * @author 茂木翼
+ */
+
+#include <Note/BaseNote.h>
+
+/// <summary>
+/// ロングノーツ
+/// </summary>
+class LongNote : public BaseNote{
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	LongNote();
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	/// <param name="modelHandle">モデルハンドル</param>
+	void Initialize(const uint32_t modelHandle)override;
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update()override;
+
+	/// <summary>
+	/// 描画
+	/// </summary>
+	/// <param name="camera"></param>
+	void DrawObject3D(const Camera& camera) override;
+
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
+	~LongNote() = default;
+
+private:
+
+
+};
+
